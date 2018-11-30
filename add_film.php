f874c5523f05f218d94c8512ca021b92b80c90a5
Mike <korobov.mike@gmail.com>
2018-11-30 23:35:06 +0500

    First updates

---
+++ add_film.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8"/>
+	<link rel="stylesheet" type="text/css" href="css/notebook.css">
+	<title>Всё что насмотрено непосильным трудом</title>
+</head>
+
+
+<body>
+	<input type="text" name="film_name" placeholder="Название фильма">
+	<input type="text" name="film_country" placeholder="Страна производства">
+	<input type="text" name="film_year" placeholder="Год выхода">
+	<input type="text" name="film_director" placeholder="Режиссёр">
+	<input type="text" name="film_genge" placeholder="Примерный жанр">
+
+<?php
+
+
+?>
+
+</body>
+</html>
